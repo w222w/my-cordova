589d81abbe0d151c9e5b670fab36ea7d99e18f6b
yuriy.tretyakov <yuriy.tretyakov@vilmate.com>
2020-05-14 20:07:49 +0300

    added init for cordova

--- www/mycordova.js
+++ www/mycordova.js
@@ -2,11 +2,21 @@ var argscheck = require('cordova/argscheck');
 var exec = require('cordova/exec');
 
 
-var myCordova = {};
-
-myCordova.version = '0.0.1';
-myCordova.getVersion = function(){
-  return this.version;
+var myCordova = {
+  version: '0.0.1',
+  getVersion: function(){
+    return this.version;
+  },
+  init: function(){
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+  onDeviceReady: function(){
+    if(navigator.splashscreen){
+    navigator.splashscreen.hide();
+    }
+  }
 };
+
+myCordova.init();
 
 module.exports = myCordova;
