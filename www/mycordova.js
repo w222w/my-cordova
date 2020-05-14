8d57262427b7db7ed1cbc6ad78428aa0513d7715
yuriy.tretyakov <yuriy.tretyakov@vilmate.com>
2020-05-14 19:03:26 +0300

    second commit

--- www/mycordova.js
+++ www/mycordova.js
@@ -1,15 +1,12 @@
-window.mycordova = (function() {
-    /*cordova.exec(callback, function(err) {
-        callback('Nothing to echo.');
-    }, "Echo", "echo", [str]);
-    */
-    
-    return {
-      version: '0.0.1',
-      getVersion(){
-      return this.version;
-      }
-    
-    };
-    
-})();
+var argscheck = require('cordova/argscheck');
+var exec = require('cordova/exec');
+
+
+var myCordova = {};
+
+myCordova.version = '0.0.1';
+myCordova.getVersion = function(){
+  return this.version;
+};
+
+module.exports = myCordova;
